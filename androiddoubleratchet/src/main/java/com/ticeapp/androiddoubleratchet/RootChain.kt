98aa6e4af0d5c6d9199b2740222f37b4cdde8c94
Fabio Tacke <fabio@tacke.berlin>
2020-08-14 14:27:48 +0200

    Update dependency

--- androiddoubleratchet/src/main/java/com/ticeapp/androiddoubleratchet/RootChain.kt
+++ androiddoubleratchet/src/main/java/com/ticeapp/androiddoubleratchet/RootChain.kt
@@ -10,12 +10,12 @@ import com.ticeapp.androidhkdf.deriveHKDFKey
 
 internal typealias ChainKey = Key
 
-internal class RootChain(var keyPair: KeyPair, var remotePublicKey: Key?, var rootKey: Key, val info: String, private val sodium: KeyExchange.Lazy) {
+internal class RootChain(var keyPair: KeyPair, var remotePublicKey: Key?, var rootKey: Key, val info: String, private val sodium: LazySodiumAndroid) {
     fun ratchetStep(side: Side): ChainKey {
         val remotePublicKey = remotePublicKey ?: throw DRError.RemotePublicKeyMissingException()
 
         val input = calculateSessionKey(keyPair, remotePublicKey, side)
-        val derivedHKDFKey = deriveHKDFKey(input, rootKey.asBytes, info, L = 64)
+        val derivedHKDFKey = deriveHKDFKey(input, rootKey.asBytes, info, L = 64, sodium = sodium)
 
         rootKey = Key.fromBytes(derivedHKDFKey.sliceArray(0..31))
         return ChainKey.fromBytes(derivedHKDFKey.sliceArray(32..63))
